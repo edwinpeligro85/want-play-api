ea07342e45f5fbfce6d2092f8fba7cd338ef58b5
Edwin Serna <edwinflow888@gmail.com>
2022-04-28 19:45:22 -0500

    Agregando Swagger

---
+++ src/app.swagger.ts
@@ -0,0 +1,18 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export const initSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Quiero Jugar')
+    .setDescription(
+      `Esta es una API para la realización de encuentros deportivos en tu ciudad. \n 
+      Para generar y descargar un archivo JSON Swagger, navegue a http://{{host}}/docs-json
+    `,
+    )
+    .setVersion('1.0')
+    .addTag('Soccer, Sport, Shop')
+    // .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initSwagger } from './app.swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Documentation API
+  initSwagger(app);
+
+  app.enableCors({
+    methods: '*',
+    origin: '*',
+    allowedHeaders: '*',
+    credentials: false,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
